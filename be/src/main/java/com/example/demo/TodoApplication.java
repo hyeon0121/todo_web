7b756d8681f2f5225d6f5c5dc0a90f8dfc1b53d1
2015005050 <dpwl1172@naver.com>
2019-09-09 18:29:50 +0900

    Fix cors error

---
+++ be/src/main/java/com/example/demo/TodoApplication.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import com.example.demo.mapper.TodoMapper;
+import com.example.demo.model.Todo;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@MapperScan("com.example.demo.mapper")
+@SpringBootApplication(scanBasePackages={
+		"com.example.demo.service", "com.example.application"})
+public class TodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET","POST", "PUT", "DELETE", "OPTIONS");
+			}
+		};
+	}
+//
+//	@Override
+//	public void run(String... args) throws Exception {
+//		System.out.println("All :" + todoMapper.findAll());
+//	}
+}
